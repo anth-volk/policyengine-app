de6770232ed33bcc90c4b9d335c27afdaf7393e5
Anthony Volk <anth.volk@gmail.com>
2024-03-27 16:09:56 +0200

    fix: Move modals to modals folder

--- src/redesign/components/PolicyEngine.jsx
+++ src/redesign/components/PolicyEngine.jsx
@@ -21,7 +21,7 @@ import Testimonials from "./Testimonials";
 import CalculatorInterstitial from "./CalculatorInterstitial";
 import CitizensEconomicCouncil from "./CitizensEconomicCouncil";
 import APIDocumentationPage from "../../pages/APIDocumentationPage";
-import CookieConsent from "layout/CookieConsent";
+import CookieConsent from "modals/CookieConsent";
 import TrafwaCalculator from "./TrafwaCalculator";
 
 const PolicyPage = lazy(() => import("../../pages/PolicyPage"));
