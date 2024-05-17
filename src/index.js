dc73f40def08d31ba708b3bab3ce251851115847
Anthony Volk <anth.volk@gmail.com>
2024-05-17 21:27:31 +0200

    eat: Use createBrowserRouter on app

--- src/index.js
+++ src/index.js
@@ -8,18 +8,29 @@ import { BrowserRouter as Router } from "react-router-dom";
 import loc_en_gb from "./lang/plotly_locales/locale-en-gb.js";
 import loc_en_us from "./lang/plotly_locales/locale-en-us.js";
 import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate.jsx";
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
 
 var Plotly = require("plotly.js/dist/plotly.js");
 Plotly.register(loc_en_gb);
 Plotly.register(loc_en_us);
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PolicyEngine />
+  }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <Auth0ProviderWithNavigate>
-      <PolicyEngine />
-    </Auth0ProviderWithNavigate>
-  </Router>,
+    <RouterProvider router={router} >
+      <Auth0ProviderWithNavigate>
+      {/*<PolicyEngine />*/}
+      </Auth0ProviderWithNavigate>
+    </RouterProvider>
 );
 
 serviceWorkerRegistration.unregister();
