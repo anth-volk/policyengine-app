a65d86e2fa73c22363a331abd6740b16c3280e49
Anthony Volk <anth.volk@gmail.com>
2024-05-17 23:58:34 +0200

    feat: Add simpler pages to new router

---
+++ src/routing/router.js
@@ -0,0 +1,143 @@
+import { createBrowserRouter } from "react-router-dom";
+import Auth0ProviderWithNavigate from "../auth/Auth0ProviderWithNavigate";
+import ErrorPage from "../layout/Error";
+import RedirectToCountry from "./RedirectToCountry";
+import CountryIdLayout from "./CountryIdLayout";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Jobs from "../pages/Jobs";
+import Testimonials from "../pages/Testimonials";
+import CalculatorInterstitial from "../pages/CalculatorInterstitial";
+import Research from "../pages/Research";
+import Donate from "../pages/Donate";
+import PrivacyPage from "../pages/PrivacyPage";
+import Contact from "../pages/Contact";
+import TACPage from "../pages/TermsAndConditions";
+import BlogPage from "../pages/BlogPage";
+import CitizensEconomicCouncil from "../applets/CitizensEconomicCouncil";
+import TrafwaCalculator from "../applets/TrafwaCalculator";
+
+export const router = createBrowserRouter([
+ {
+    element: <Auth0ProviderWithNavigate />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <RedirectToCountry />
+      },
+      {
+        path: "/:countryId",
+        element: <CountryIdLayout />,
+        children: [
+          {
+            index: true,
+            element: <Home />
+          },
+          {
+            path: "about",
+            element: <About />
+          },
+          {
+            path: "jobs",
+            element: <Jobs />
+          },
+          {
+            path: "testimonials",
+            element: <Testimonials />
+          },
+          {
+            path: "calculator",
+            element: <CalculatorInterstitial />
+          },
+          {
+            path: "research",
+            element: <Research />,
+          },
+          {
+            path: "donate",
+            element: <Donate />
+          },
+          {
+            path: "privacy",
+            element: <PrivacyPage />
+          },
+          // Note: The contact page is deprecated,
+          // but removing it is out of the scope
+          // of the PR creating this router
+          {
+            path: "conatct",
+            element: <Contact />
+          },
+          {
+            path: "terms",
+            element: <TACPage />
+          },
+        ]
+        
+      },
+      {
+        path: "/uk",
+        children: [
+          {
+            path: "cec",
+            element: <CitizensEconomicCouncil />
+          }
+        ]
+      },
+      {
+        path: "/us",
+        children: [
+          {
+            path: "trafwa-ctc-calculator",
+            element: <TrafwaCalculator />
+          }
+        ]
+      }
+    ]
+ }
+]);
+
+/*
+
+        <Route path="/callback" element={<AuthCallback />} />
+        <Route path="/:countryId/research/*" element={<BlogPage />} />
+
+        <Route
+          path="/:countryId/household/*"
+          element={metadata ? householdPage : error ? errorPage : loadingPage}
+        />
+        <Route
+          path="/:countryId/policy/*"
+          element={metadata ? policyPage : error ? errorPage : loadingPage}
+        />
+
+        <Route
+          path="/:countryId/profile"
+          element={
+            <Navigate to={`/${countryId}/profile/${userProfile.user_id}`} />
+          }
+        />
+        <Route
+          path="/:countryId/profile/:user_id"
+          element={
+            <UserProfilePage
+              metadata={metadata}
+              authedUserProfile={userProfile}
+            />
+          }
+        />
+
+*/
+        {/* redirect from /countryId/blog/slug to /countryId/research/slug */}
+        /*
+        <Route
+          path="/:countryId/blog/:slug"
+          element={<Navigate to={`/${countryId}/research/${pathParts[3]}`} />}
+        />
+        */
+
+        {/* Redirect for unrecognized paths */}
+        /*
+        <Route path="*" element={<Navigate to={`/${countryId}`} />} />
+        */
